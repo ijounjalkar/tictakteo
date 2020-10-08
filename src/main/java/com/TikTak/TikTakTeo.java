899e7366f70a0daa354909e36d12cd39f5ba12fe
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-08 10:27:00 +0530

    Print Board

--- src/main/java/com/TikTak/TikTakTeo.java
+++ src/main/java/com/TikTak/TikTakTeo.java
@@ -5,27 +5,43 @@ import java.util.Scanner;
 public class TikTakTeo { 
 	public static void main(String[] args) {
 		char[] board = createBoard();
-		input();
+		inputXorO();
 		
 	}
 	private static char[] createBoard() {
 		char[] board = new char[10];
-		for(int i=0; i<board.length; i++) {
+		for (int i=0; i<board.length; i++) {
 		board[i] = ' ';
 	}
 	return board;
 }
-	public static void input() {
-		char player,computer;
+	public static void inputXorO() {
+		char player , computer;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the input");
 		player = input.next().charAt(0);
-		if (player == 'X') {
+		if(player == 'X' || player == 'O') {
+		  if (player == 'X') {
 			computer = 'O';
 		}
 		else {
 			computer = 'X';
 		}
+	
 	}
+	else {
+		System.out.println("Invalid Choice. Please enter either (X/O)");
+		inputXorO();
+	}
+	}
+	public static void showBoard () {
+		String[] board = null;
+		for(int i=1; i<board.length; i++) {
+		System.out.println(board[i] + " | ");
+		if(i % 3 == 0)
+			System.out.println();
+		}
+	}
+
 
 }
