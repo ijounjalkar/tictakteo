975acae3f52da4afa8d9dbd57a1ad4a59c4db8f5
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-08 09:25:19 +0530

    Board is made

--- src/main/java/com/TikTak/TikTakTeo.java
+++ src/main/java/com/TikTak/TikTakTeo.java
@@ -1,5 +1,18 @@
 package com.TikTak;
 
-public class TikTakTeo {
+import java.util.Scanner;
+
+public class TikTakTeo { 
+	public static void main(String[] args) {
+		char[] board = createBoard();
+		
+	}
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i=0; i<board.length; i++) {
+		board[i] = ' ';
+	}
+	return board;
+}
 
 }
